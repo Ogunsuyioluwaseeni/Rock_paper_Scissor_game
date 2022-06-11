36cba66f229e9a0e6afe0060489e2fdb1cd2e397
Ogunsuyioluwaseeni <olamy2292@gmail.com>
2022-06-12 00:01:55 +0100

    new changes

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import random
-
 while True:
     Player = input(
         'Enter Your weapon: R for Rock, P for Paper, S for Scissors=>:')
     possible_selection = ['R', 'P', 'S']
     CPU = random.choice(possible_selection)
     print(f"\n Player ({Player.upper()})  : CPU ({CPU})")
-    if (Player.upper() == CPU):
+    if Player.upper() not in possible_selection:
+       print("Error!!! Enter the correct letter")
+       print("R,P,S") 
+       continue
+    elif (Player.upper() == CPU):
         print("It's a tie ")
         continue
-    elif (Player.upper() == 'R' and CPU == 'S'
+    if (Player.upper() == 'R' and CPU == 'S'
           or Player.upper() == 'P' and CPU == 'R'
           or Player.upper() == 'S' and CPU == 'P'):
         print(" You Win!!! Weldone Player")
-
-    elif (Player.upper() == 'R' and CPU == 'P'
-          or Player.upper() == 'P' and CPU == 'S'
-          or Player.upper() == 'S' and CPU == 'R'):
-        print("Oh wow CPU Win!!!")
     else:
-        print("Error!!! Enter the correct letter")
-        print("R,P,S")
+        print("Oh wow CPU Win!!!")
     play_again = input("Do you want to play again? (y/n): ")
     if play_again.lower() != "y":
-        break
+        quit()
